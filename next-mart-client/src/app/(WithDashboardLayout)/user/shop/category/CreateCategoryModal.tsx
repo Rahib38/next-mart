30f33a12b1dcafad4577f55689e39d6ad2462407
Nadimul Rahib <nadimulrahib38@gmail.com>
2025-02-23 22:12:34 -0800

    added

--- next-mart-client/src/app/(WithDashboardLayout)/user/shop/category/CreateCategoryModal.tsx
+++ next-mart-client/src/app/(WithDashboardLayout)/user/shop/category/CreateCategoryModal.tsx
@@ -10,7 +10,7 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import {
-    Form,
+  Form,
   FormControl,
   FormField,
   FormItem,
@@ -25,28 +25,28 @@ import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import { toast } from "sonner";
 
 const CreateCategoryModal = () => {
-      const [imageFiles, setImageFiles] = useState<File[] | []>([]);
-      const [imagePreview, setImagePreview] = useState<string[] | []>([]);
+  const [imageFiles, setImageFiles] = useState<File[] | []>([]);
+  const [imagePreview, setImagePreview] = useState<string[] | []>([]);
   const form = useForm();
   const {
     formState: { isSubmitted },
   } = form;
   const onSubmit: SubmitHandler<FieldValues> = async (data) => {
-try{
-const formData=new FormData()
-formData.append('data',JSON.stringify(data))
-formData.append('icon',imageFiles[0] as File)
-const res= await createCategory(formData)
-console.log(res)
-console.log(res?.errorSources[0]?.message)
-if(res.success){
-  toast.success(res?.message)
-}else{
-  toast.error(res?.errorSources[0]?.message)
-}
-} catch(err:any){
-  console.log(err)
-} };
+    try {
+      const formData = new FormData();
+      formData.append("data", JSON.stringify(data));
+      formData.append("icon", imageFiles[0] as File);
+      const res = await createCategory(formData);
+   
+      if (res.success) {
+        toast.success(res?.message);
+      } else {
+        toast.error(res?.errorSources[0]?.message);
+      }
+    } catch (err: any) {
+      console.log(err);
+    }
+  };
   return (
     <div>
       <Dialog>
@@ -57,64 +57,64 @@ if(res.success){
           <DialogHeader>
             <DialogTitle>Create Product Category</DialogTitle>
           </DialogHeader>
-      <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)}>
-            <FormField
-              control={form.control}
-              name="name"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Name</FormLabel>
-                  <FormControl>
-                    <Input type="name" {...field} value={field.value || ""} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-         <div className="flex justify-between flex-col md:flex-row gap-2 mt-4 items-center">
-            <div className="col-span-4 md:col-span-3">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
               <FormField
                 control={form.control}
-                name="description"
+                name="name"
                 render={({ field }) => (
                   <FormItem>
-                    <FormLabel>Description</FormLabel>
+                    <FormLabel>Name</FormLabel>
                     <FormControl>
-                      <Textarea
-                        className="h-36 w-72"
-                        {...field}
-                        value={field.value || ""}
-                      />
+                      <Input type="name" {...field} value={field.value || ""} />
                     </FormControl>
                     <FormMessage />
                   </FormItem>
                 )}
               />
-            </div>
+              <div className="flex justify-between flex-col md:flex-row gap-2 mt-4 items-center">
+                <div className="col-span-4 md:col-span-3">
+                  <FormField
+                    control={form.control}
+                    name="description"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Description</FormLabel>
+                        <FormControl>
+                          <Textarea
+                            className="h-36 w-72"
+                            {...field}
+                            value={field.value || ""}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
 
-            <div className="mt-8">
-              {imagePreview.length > 0 ? (
-                <ImagePreviewer
-                  imagePreview={imagePreview}
-                  setImagePreview={setImagePreview}
-                  setImageFiles={setImageFiles}
-                />
-              ) : (
-                <NMImageUploader
-                  setImagePreview={setImagePreview}
-                  setImageFiles={setImageFiles}
-                  label="Upload icon"
-                />
-              )}
-            </div>
-          </div>
+                <div className="mt-8">
+                  {imagePreview.length > 0 ? (
+                    <ImagePreviewer
+                      imagePreview={imagePreview}
+                      setImagePreview={setImagePreview}
+                      setImageFiles={setImageFiles}
+                    />
+                  ) : (
+                    <NMImageUploader
+                      setImagePreview={setImagePreview}
+                      setImageFiles={setImageFiles}
+                      label="Upload icon"
+                    />
+                  )}
+                </div>
+              </div>
 
-            <Button type="submit" className="mt-5 w-full">
-              {isSubmitted ? "Loging.........." : "Login"}
-            </Button>
-          </form>
-      </Form>
+              <Button type="submit" className="mt-5 w-full">
+                {isSubmitted ? "Loging.........." : "Login"}
+              </Button>
+            </form>
+          </Form>
         </DialogContent>
       </Dialog>
     </div>
